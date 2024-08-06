f5a5e0c6abe151728e58209c115acfb676c3fbbe
JayeshW <jayeshwankhede28@gmail.com>
2024-08-07 01:14:44 +0530

    Navbar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,5 @@ import { UserButton } from "@clerk/nextjs";
 
 export default async function Home() {
   const movies = await trpcServer.movies.movies.query();
-  return (
-    <main className="">
-      Hello
-      <UserButton />
-      <div>
-        {movies.map((movie) => (
-          <div key={movie.id}>
-            <div>{movie.title}</div>
-            <div>{movie.duration} mins</div>
-            <div>{movie.genre}</div>
-            <div>{movie.director}</div>
-          </div>
-        ))}
-      </div>
-    </main>
-  );
+  return <main className="bg-gray-400"></main>;
 }
