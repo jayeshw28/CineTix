c8b4113559a9fbbec0b567cb3a6a68e11f0c5c6b
JayeshW <jayeshwankhede28@gmail.com>
2024-08-10 00:58:10 +0530

    Admin console added

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,38 @@
+import { TellThem } from "@/components/molecules/TellThem";
+import { AdminMenu } from "@/components/organisms/AdminMenu";
+import { SimpleSidebar } from "@/components/molecules/SimpleSidebar";
+import { trpcServer } from "@/trpc/clients/server";
+import { auth } from "@clerk/nextjs/server";
+import { Link } from "lucide-react";
+
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { userId } = auth();
+  if (!userId) {
+    return <Link href="/sign-in">Sign-In</Link>;
+  }
+  const adminMe = await trpcServer.admins.adminMe.query();
+  if (!adminMe?.id) {
+    return <TellThem role={"admin"} uid={`${userId}`} />;
+  }
+
+  return (
+    <div className="flex mt-2 ">
+      <div className="hidden w-full max-w-xs min-w-min sm:block">
+        <AdminMenu />
+      </div>
+
+      <div className="flex-grow ">
+        <div className="sm:hidden">
+          <SimpleSidebar>
+            <AdminMenu />
+          </SimpleSidebar>
+        </div>
+        <div className="p-4 bg-gray-100">{children}</div>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,5 @@ import { trpcServer } from "@/trpc/clients/server";
 import { UserButton } from "@clerk/nextjs";
 
 export default async function Home() {
-  const movies = await trpcServer.movies.movies.query();
   return <main className="bg-gray-400"></main>;
 }
